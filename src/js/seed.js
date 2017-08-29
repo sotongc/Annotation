987f3cd1f8d6d05a916055cb745e8a1fb635f4ee
张新杰 <18355335380@163.com>
2017-08-29 19:56:19 +0800

    add multiple country

--- src/js/seed.js
+++ src/js/seed.js
@@ -95,7 +95,7 @@ let __pannel=new $pannel({
 	el:'#pannel',
 	data:{
 		type:0,
-		countries:['all','ke','ng','za','tz','gh','in','id','hindi'],
+		countries:['all','ke','ng','za','tz','gh','in','id','hindi','ru','ua','by'],
 		category:['all','political','sports','entertainment','game'],
 		query:query
 	}
